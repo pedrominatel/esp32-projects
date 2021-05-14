724476b96094682a0b43215ec68009b4efcfa521
pedro.minatel <pedro.minatel@espressif.com>
2021-05-14 14:48:07 +0100

    Project updates

--- demo/esp32/lvgl_basic/main/lvgl_basic.c
+++ demo/esp32/lvgl_basic/main/lvgl_basic.c
@@ -1,17 +1,13 @@
-// Copyright 2015-2021 Espressif Systems (Shanghai) CO LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+/* LVGL Example project
+ *
+ * Basic project to test LVGL on ESP32 based projects.
+ *
+ * This example code is in the Public Domain (or CC0 licensed, at your option.)
+ *
+ * Unless required by applicable law or agreed to in writing, this
+ * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ */
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,8 +20,6 @@
 #include "esp_system.h"
 #include "driver/gpio.h"
 
-#include "esp_log.h"
-
 /* Littlevgl specific */
 #ifdef LV_LVGL_H_INCLUDE_SIMPLE
 #include "lvgl.h"
@@ -35,36 +29,182 @@
 
 #include "lvgl_helpers.h"
 
-const static char *TAG = "LVGL";
+#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+    #if defined CONFIG_LV_USE_DEMO_WIDGETS
+        #include "lv_examples/src/lv_demo_widgets/lv_demo_widgets.h"
+    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
+        #include "lv_examples/src/lv_demo_keypad_encoder/lv_demo_keypad_encoder.h"
+    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
+        #include "lv_examples/src/lv_demo_benchmark/lv_demo_benchmark.h"
+    #elif defined CONFIG_LV_USE_DEMO_STRESS
+        #include "lv_examples/src/lv_demo_stress/lv_demo_stress.h"
+    #else
+        #error "No demo application selected."
+    #endif
+#endif
 
+/*********************
+ *      DEFINES
+ *********************/
+#define TAG "demo"
+#define LV_TICK_PERIOD_MS 1
 
-void app_main(void)
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+static void lv_tick_task(void *arg);
+static void guiTask(void *pvParameter);
+static void create_demo_application(void);
+
+/**********************
+ *   APPLICATION MAIN
+ **********************/
+void app_main() {
+
+    /* If you want to use a task to create the graphic, you NEED to create a Pinned task
+     * Otherwise there can be problem such as memory corruption and so on.
+     * NOTE: When not using Wi-Fi nor Bluetooth you can pin the guiTask to core 0 */
+    xTaskCreatePinnedToCore(guiTask, "gui", 4096*2, NULL, 0, NULL, 1);
+}
+
+/* Creates a semaphore to handle concurrent call to lvgl stuff
+ * If you wish to call *any* lvgl function from other threads/tasks
+ * you should lock on the very same semaphore! */
+SemaphoreHandle_t xGuiSemaphore;
+
+static void guiTask(void *pvParameter) {
+
+    (void) pvParameter;
+    xGuiSemaphore = xSemaphoreCreateMutex();
+
+    lv_init();
+
+    /* Initialize SPI or I2C bus used by the drivers */
+    lvgl_driver_init();
+
+    lv_color_t* buf1 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    assert(buf1 != NULL);
+
+    /* Use double buffered when not working with monochrome displays */
+#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+    lv_color_t* buf2 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    assert(buf2 != NULL);
+#else
+    static lv_color_t *buf2 = NULL;
+#endif
+
+    static lv_disp_buf_t disp_buf;
+
+    uint32_t size_in_px = DISP_BUF_SIZE;
+
+#if defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_IL3820         \
+    || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_JD79653A    \
+    || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_UC8151D     \
+    || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_SSD1306
+
+    /* Actual size in pixels, not bytes. */
+    size_in_px *= 8;
+#endif
+
+    /* Initialize the working buffer depending on the selected display.
+     * NOTE: buf2 == NULL when using monochrome displays. */
+    lv_disp_buf_init(&disp_buf, buf1, buf2, size_in_px);
+
+    lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.flush_cb = disp_driver_flush;
+
+    /* When using a monochrome display we need to register the callbacks:
+     * - rounder_cb
+     * - set_px_cb */
+#ifdef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+    disp_drv.rounder_cb = disp_driver_rounder;
+    disp_drv.set_px_cb = disp_driver_set_px;
+#endif
+
+    disp_drv.buffer = &disp_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    /* Register an input device when enabled on the menuconfig */
+#if CONFIG_LV_TOUCH_CONTROLLER != TOUCH_CONTROLLER_NONE
+    lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.read_cb = touch_driver_read;
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    lv_indev_drv_register(&indev_drv);
+#endif
+
+    /* Create and start a periodic timer interrupt to call lv_tick_inc */
+    const esp_timer_create_args_t periodic_timer_args = {
+        .callback = &lv_tick_task,
+        .name = "periodic_gui"
+    };
+    esp_timer_handle_t periodic_timer;
+    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
+
+    /* Create the demo application */
+    create_demo_application();
+
+    while (1) {
+        /* Delay 1 tick (assumes FreeRTOS tick is 10ms */
+        vTaskDelay(pdMS_TO_TICKS(10));
+
+        /* Try to take the semaphore, call lvgl related function on success */
+        if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY)) {
+            lv_task_handler();
+            xSemaphoreGive(xGuiSemaphore);
+       }
+    }
+
+    /* A task should NEVER return */
+    free(buf1);
+#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+    free(buf2);
+#endif
+    vTaskDelete(NULL);
+}
+
+static void create_demo_application(void)
 {
-    // TaskHandle_t bsp_display_task_handle;
-    // int ret = xTaskCreate(bsp_display_task, "bsp_display", 4096 * 2, xTaskGetCurrentTaskHandle(), 3, &bsp_display_task_handle);
-    // assert(ret == pdPASS);
+    /* When using a monochrome display we only show "Hello World" centered on the
+     * screen */
+#if defined CONFIG_LV_TFT_DISPLAY_MONOCHROME || \
+    defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_ST7735S
 
-    // /* Wait until LVGL is ready */
-    // if (ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(1000))) {
-    //     vTaskSuspend(bsp_display_task_handle);
-    //     /*Create a window*/
-    //     lv_obj_t *win = lv_win_create(lv_scr_act(), NULL);
-    //     lv_win_set_title(win, "ESP-WROVER-KIT:\nBoard Support Package example");
-    //     lv_win_set_scrollbar_mode(win, LV_SCROLLBAR_MODE_OFF);
+    /* use a pretty small demo for monochrome displays */
+    /* Get the current screen  */
+    lv_obj_t * scr = lv_disp_get_scr_act(NULL);
 
-    //     /* Container */
-    //     lv_obj_t *cont = lv_cont_create(win, NULL);
-    //     lv_cont_set_fit(cont, LV_FIT_TIGHT);
-    //     lv_cont_set_layout(cont, LV_LAYOUT_COLUMN_LEFT);
+    /*Create a Label on the currently active screen*/
+    lv_obj_t * label1 =  lv_label_create(scr, NULL);
 
-    //     /* Checkboxes */
-    //     lv_obj_t *recording_checkbox = lv_checkbox_create(cont, NULL);
-    //     lv_checkbox_set_text(recording_checkbox, "Checkbox 1");
-    //     lv_obj_t *playing_checkbox = lv_checkbox_create(cont, NULL);
-    //     lv_checkbox_set_text(playing_checkbox, "Checkbox 2");
+    /*Modify the Label's text*/
+    lv_label_set_text(label1, "Hello\nworld");
 
-    //     vTaskResume(bsp_display_task_handle);
-    // } else {
-    //     ESP_LOGW(TAG, "LVGL init timed out");
-    // }
+    /* Align the Label to the center
+     * NULL means align on parent (which is the screen now)
+     * 0, 0 at the end means an x, y offset after alignment*/
+    lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
+#else
+    /* Otherwise we show the selected demo */
+
+    #if defined CONFIG_LV_USE_DEMO_WIDGETS
+        lv_demo_widgets();
+    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
+        lv_demo_keypad_encoder();
+    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
+        lv_demo_benchmark();
+    #elif defined CONFIG_LV_USE_DEMO_STRESS
+        lv_demo_stress();
+    #else
+        #error "No demo application selected."
+    #endif
+#endif
+}
+
+static void lv_tick_task(void *arg) {
+    (void) arg;
+
+    lv_tick_inc(LV_TICK_PERIOD_MS);
 }
