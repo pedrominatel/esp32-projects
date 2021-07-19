f0050ca34fec5a91a7a4489bc78a9eb67a69b8de
pedro.minatel <pedro.minatel@espressif.com>
2021-07-19 17:31:18 +0100

    Example for LoRa Lib

--- demo/lora/lora-demo-lib/main/lora-demo-lib.c
+++ demo/lora/lora-demo-lib/main/lora-demo-lib.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "lora.h"
 
-void app_main(void)
+void task_tx(void *p)
 {
+    for(;;) {
+        vTaskDelay(pdMS_TO_TICKS(5000));
+        lora_send_packet((uint8_t*)"Hello", 5);
+        printf("packet sent...\n");
+    }
+}
 
+void app_main()
+{
+    lora_init();
+    lora_set_frequency(915e6);
+    lora_enable_crc();
+    xTaskCreate(&task_tx, "task_tx", 2048, NULL, 5, NULL);
 }
